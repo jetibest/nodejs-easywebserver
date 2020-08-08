929f95ff7630b94ee4b1f3b98be3abd5fb410e21
Master Yeti <developer@masteryeti.com>
2020-08-08 13:54:32 +0200

    Bugfix forcedir.

--- mod-forcedir.js
+++ mod-forcedir.js
@@ -6,6 +6,7 @@ module.exports = function(options)
 	this.middleware = function(req, res, next)
 	{
 		var originalPath = req.headers[header];
+		var isExternal = false;
 		if(typeof originalPath !== 'string')
 		{
 			originalPath = mod._options._easywebserver.getPath(req);
@@ -14,6 +15,7 @@ module.exports = function(options)
 		{
 			originalPath = originalPath.replace(/[?#].*$/gi, ''); // ensure path from header does not contain querystring or hash
 			originalPath = originalPath.replace(/^[^/]+:\/\//gi, '').replace(/^[^/]*/gi, ''); // remove the protocol/hostname/port
+			isExternal = true;
 		}
 		// if using a proxy, it has to provide the original path in a header
 		// redirect /app to /app/, ensure a trailing slash
@@ -23,7 +25,14 @@ module.exports = function(options)
 		// if trailing slash, or a dot (extension) is found, then do not redirect
 		if(originalPath.indexOf('.') !== -1 || originalPath.substr(-1) === '/') return next();
 		
-		req.url = mod._options._easywebserver.replaceURLPath(p => p + '/', req);
+		if(isExternal)
+		{
+			req.url = mod._options._easywebserver.replaceURLPath(originalPath + '/', req);
+		}
+		else
+		{
+			req.url = mod._options._easywebserver.replaceURLPath(p => p.replace(/\/$/g, '') + '/', req);
+		}
 		
 		res.redirect(302, req.url);
 		next();
