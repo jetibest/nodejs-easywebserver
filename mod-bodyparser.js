248880fb3d0681e8a016a8fc0bea8008cba20f80
jetibest <jetibest@github.com>
2024-02-04 15:11:35 +0100

    Bodyparser fixes.

--- mod-bodyparser.js
+++ mod-bodyparser.js
@@ -8,33 +8,48 @@ module.exports = function(options)
 	// The difference with mod-urlrewrite is that this function does an internal redirect back to the start
 	var limit = options.limit || '16mb';
 	var parsers = options.parsers || [
-        'json',
-        'urlencoded'
-    ];
-    if(typeof parsers === 'string') parsers = parsers.split(',').map(str => str.trim()).filter(str => str.length > 0);
-    parsers = parsers.map(parser =>
-        typeof parser === 'function' ? parser : bodyParser[parser].apply(bodyParser[parser], parser === 'urlencoded' ? [{limit: limit, extended: false}] : [{limit: limit}]);
-    );
+		'json',
+		'urlencoded'
+	];
+	if(typeof parsers === 'string') parsers = parsers.split(',').map(str => str.trim()).filter(str => str.length > 0);
+	parsers = parsers.map(parser =>
+	{
+		if(typeof parser === 'function')
+		{
+			return parser;
+		}
+		else
+		{
+			var parser_args = [{limit: limit}];
+			if(parser === 'urlencoded')
+			{
+				parser_args.extended = false;
+			}
+			return bodyParser[parser].apply(bodyParser[parser], parser_args);
+		}
+	});
 	
 	this.group = 'pre-process';
-	this.middleware = (req, res, next) =>
+	this.middleware = async (req, res, next) =>
 	{
 		if(!res || res.headersSent || res.statusCode !== 200) return next();
 		
 		for(var i=0;i<parsers.length;++i)
 		{
-            await new Promise((resolve, reject) =>
-            {
-                try
-                {
-                    parsers[i].call(this, req, res, resolve);
-                }
-                catch(err)
-                {
-                    reject(err);
-                }
-            });
+			await new Promise((resolve, reject) =>
+			{
+				try
+				{
+					parsers[i].call(this, req, res, resolve);
+				}
+				catch(err)
+				{
+					reject(err);
+				}
+			});
 		}
+
+		next();
 	};
 	
 	console.log('mod-bodyparser initialized with ' + parsers.length + ' bodyparser instances, with maximum request body size limit: ' + limit);
